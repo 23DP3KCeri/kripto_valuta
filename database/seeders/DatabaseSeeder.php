0f4bfd5694cce4453c6e80cc6a319881df7d975d
Kristers Cerins-Pakse <A230472KC@rvt.lv>
2026-05-15 00:47:27 +0300

    test users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,25 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::create([
+            'name' => 'User1',
+            'email' => 'user1@gmail.com',
+            'password' => Hash::make('test1234'),
+            'role' => 'user',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'User2',
+            'email' => 'user2@gmail.com',
+            'password' => Hash::make('test1234'),
+            'role' => 'admin',
         ]);
 
         $this->call(ExchangeRateSeeder::class);
